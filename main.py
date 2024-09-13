0a9bbfa3a4c34c878a23f5bc733b1de2650d365b
Will Pagano <will@silk.security>
2024-09-13 12:09:23 -0400

    more stuff

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 
 
+import base64
+
+
 def main():
     my_cool_input = input('Enter something fun')
     eval(my_cool_input)
+
+def another_function():
+    pass
+
+def code_execution(request):
+    "lifted from docs hopefully triggers a problem"
+    if request.method == 'POST':
+        first_name = base64.decodestring(request.POST.get('first_name', ''))
+        #BAD -- Allow user to define code to be run.
+        exec("setname('%s')" % first_name)
+        
+if __name__ == '__main__':
+    main()
